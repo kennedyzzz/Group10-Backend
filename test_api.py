99c74001aea4f1447b5705ca29456337ca0eb90b
George Gathogo <gathogogeorge11@gmail.com>
2024-08-02 16:25:34 +0300

    Added catalog and cart tests

--- test_api.py
+++ test_api.py
@@ -29,3 +29,18 @@ def test_home():
     assert response.status_code == 200
     assert "welcome_message" in response.json()
 
+def test_catalog():
+    url = f"{BASE_URL}/catalog"
+    response = requests.get(url)
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+def test_cart_add():
+    url = f"{BASE_URL}/cart/add"
+    data = {
+        "product_id": 1,
+        "quantity": 2
+    }
+    response = requests.post(url, json=data)
+    assert response.status_code == 200
+    assert response.json().get("message") == "Product added to cart"
