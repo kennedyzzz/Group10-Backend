f61238d73ffaed8325b97d09f23a0a77d7bb577c
George Gathogo <gathogogeorge11@gmail.com>
2024-08-02 16:24:38 +0300

    added tests for home and login page

--- test_api.py
+++ test_api.py
@@ -12,3 +12,20 @@ def test_signup():
     response = requests.post(url, json=data)
     assert response.status_code == 201
     assert response.json().get("message") == "User created successfully"
+
+    def test_login():
+     url = f"{BASE_URL}/login"
+    data = {
+        "username": "testuser",
+        "password": "testpassword"
+    }
+    response = requests.post(url, json=data)
+    assert response.status_code == 200
+    assert "token" in response.json()
+
+def test_home():
+    url = f"{BASE_URL}/home"
+    response = requests.get(url)
+    assert response.status_code == 200
+    assert "welcome_message" in response.json()
+
